f85caef8f82be2a24258ff9dd3bb7e4a35b02045
Fadyy22 <fadyalaa441@gmail.com>
2023-04-23 09:40:13 -0700

    third task

--- functions0.c
+++ functions0.c
@@ -100,28 +100,30 @@ int print_percent(va_list type, char buff[],
 
 /**
  * print_int - Print int
- * @types: Lista of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
+ *
+ * @type: List of arguments
+ * @buff: Buffer array to handle print
+ * @flag: Calculates active flags
  * @width: get width.
  * @precision: Precision specification
  * @size: Size specifier
- * Return: Number of chars printed
+ *
+ * Return: Number of chars
  */
-int print_int(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+int print_int(va_list type, char buff[],
+	int flag, int width, int precision, int size)
 {
 	int i = BUFF_SIZE - 2;
 	int is_negative = 0;
-	long int n = va_arg(types, long int);
+	long int n = va_arg(type, long int);
 	unsigned long int num;
 
 	n = convert_size_number(n, size);
 
 	if (n == 0)
-		buffer[i--] = '0';
+		buff[i--] = '0';
 
-	buffer[BUFF_SIZE - 1] = '\0';
+	buff[BUFF_SIZE - 1] = '\0';
 	num = (unsigned long int)n;
 
 	if (n < 0)
@@ -132,12 +134,59 @@ int print_int(va_list types, char buffer[],
 
 	while (num > 0)
 	{
-		buffer[i--] = (num % 10) + '0';
+		buff[i--] = (num % 10) + '0';
 		num /= 10;
 	}
 
 	i++;
 
-	return (write_number(is_negative, i, buffer, flags, width, precision, size));
+	return (write_number(is_negative, i, buffe, flag, width, precision, size));
+}
+
+/**
+ * print_binary - Prints an unsigned number to binary
+ *
+ * @type: List of arguments
+ * @buff: Buffer array to handle print
+ * @flag: Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ *
+ * Return: Numbers of char printed
+ */
+int print_binary(va_list type, char buff[],
+	int flag, int width, int precision, int size)
+{
+	unsigned int n, m, i, sum;
+	unsigned int a[32];
+	int count;
+
+	UNUSED(buff);
+	UNUSED(flag);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	n = va_arg(type, unsigned int);
+	m = 2147483648; /* (2 ^ 31) */
+	a[0] = n / m;
+	for (i = 1; i < 32; i++)
+	{
+		m /= 2;
+		a[i] = (n / m) % 2;
+	}
+	for (i = 0, sum = 0, count = 0; i < 32; i++)
+	{
+		sum += a[i];
+		if (sum || i == 31)
+		{
+			char z = '0' + a[i];
+
+			write(1, &z, 1);
+			count++;
+		}
+	}
+	return (count);
 }
 

6eef5b6487605cf59df6806873790025b0616f21
Fadyy22 <fadyalaa441@gmail.com>
2023-04-23 07:37:24 -0700

    First task

---
+++ main.h
@@ -0,0 +1,29 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define UNUSED(x) (void)(x)
+
+#define F_MINUS 1
+
+int _printf(const char *format, ...);
+
+
+/**
+ * function0.c
+ */
+int print_char(va_list type, char buff[],
+	int flag, int width, int precision, int size);
+int print_string(va_list type, char buff[],
+	int flag, int width, int precision, int size);
+int print_percent(va_list type, char buff[],
+	int flag, int width, int precision, int size);
+
+
+int handle_write_char(char c, char buff[],
+	int flag, int width, int precision, int size);
+
+#endif

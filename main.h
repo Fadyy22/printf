db25c8043dc9dd52e89c5487fcf2e50f0d322259
OmarSoliman74 <mingosos13@gmail.com.com>
2023-04-23 08:13:54 -0700

    seconed task

--- main.h
+++ main.h
@@ -1,17 +1,11 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
-
 #define UNUSED(x) (void)(x)
-
 #define F_MINUS 1
-
 int _printf(const char *format, ...);
-
-
 /**
  * function0.c
  */
@@ -21,9 +15,25 @@ int print_string(va_list type, char buff[],
 	int flag, int width, int precision, int size);
 int print_percent(va_list type, char buff[],
 	int flag, int width, int precision, int size);
-
-
 int handle_write_char(char c, char buff[],
 	int flag, int width, int precision, int size);
+
+
+/* function1.c */
+int print_int(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_binary(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_unsigned(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_octal(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_hexadecimal(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_hexa_upper(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+int print_hexa(va_list types, char map_to[],
+char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
 #endif
